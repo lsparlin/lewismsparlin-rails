4b267864b187eec641f075c54e7f82d112fe2487
Lewis Sparlin <lewis@edatasource.com>
2020-02-26 17:53:46 -0600

    Added some partial view functionality

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,9 +4,14 @@ class HomeController < ApplicationController
 
   def index
     pris_api = Prismic.api('https://lewismsparlin.prismic.io/api')
-    landing_doc = pris_api.query(Prismic::Predicates.at('document.type', 'site-header'))
 
-    @prismic_doc = landing_doc.results.first
+    landing_res = pris_api.query(Prismic::Predicates.at('document.type', 'site-header'))
+    social_res = pris_api.query(Prismic::Predicates.at('document.type', 'social-link'))
+    blogs_res = pris_api.query(Prismic::Predicates.at('document.type', 'blog-post'))
+
+    @site_header_doc = landing_res.results.first
+    @social_link_docs = social_res.results
+    @blog_post_docs = blogs_res.results
   end
 
 end
