1de3dc4f0874c747aafa947bde1e8b7ead0566d9
Lewis Sparlin <lewis@edatasource.com>
2020-02-26 15:19:25 -0600

    proved simlicity of integrating prismic.io

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
+require 'prismic'
+
 class HomeController < ApplicationController
+
   def index
+    pris_api = Prismic.api('https://lewismsparlin.prismic.io/api')
+    landing_doc = pris_api.query(Prismic::Predicates.at('document.type', 'site-header'))
+
+    @prismic_doc = landing_doc.results.first
   end
+
 end
