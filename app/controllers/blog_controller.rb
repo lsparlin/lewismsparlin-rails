90b32392208358ebeea663cdaacd95f2d6f340fb
Lewis Sparlin <lewis@edatasource.com>
2020-02-26 22:41:05 -0600

    getting some content loading

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,4 +1,15 @@
 class BlogController < ApplicationController
+
   def show
+    pris_api = Prismic.api('https://lewismsparlin.prismic.io/api')
+
+    landing_res = pris_api.query(Prismic::Predicates.at('document.type', 'site-header'))
+    social_res = pris_api.query(Prismic::Predicates.at('document.type', 'social-link'))
+
+    @blog_doc = pris_api.query(Prismic::Predicates.at('my.blog-post.uid', params[:id])).results.first
+
+    @site_header_doc = landing_res.results.first
+    @social_link_docs = social_res.results
   end
+
 end
