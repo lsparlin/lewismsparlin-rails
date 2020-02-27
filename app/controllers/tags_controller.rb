0f3fd7cf3cd72b712aa4ee4c3c0e8e91c93f17e4
Lewis Sparlin <lewis@edatasource.com>
2020-02-27 07:23:47 -0600

    Added skeleton css lib

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,5 +1,13 @@
 class TagsController < ApplicationController
+
   def index
+    pris_api = Prismic.api('https://lewismsparlin.prismic.io/api')
+
+    landing_res = pris_api.query(Prismic::Predicates.at('document.type', 'site-header'))
+    social_res = pris_api.query(Prismic::Predicates.at('document.type', 'social-link'))
+
+    @site_header_doc = landing_res.results.first
+    @social_link_docs = social_res.results
   end
 
   def show
