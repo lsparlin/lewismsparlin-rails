984534fdbd6746f978cfec4a30a25a352263fca5
Lewis Sparlin <lewis@edatasource.com>
2020-02-27 21:20:21 -0600

    view title and soem blog styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def title 
+    if content_for?(:title)
+      content_for :title
+    else
+      "#{controller_name.capitalize } | Lewis M Sparlin"
+    end
+  end
+
 end
