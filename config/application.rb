d1861cbe0f8ede87b255cb775d7b6ba5213d9bee
Lewis Sparlin <lewis.sparlin@icloud.com>
2020-07-11 11:02:08 -0500

    Add monolisa font

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,9 @@ module LewismsparlinRails
 
     config.exceptions_app = self.routes
 
+    # Font Assets
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
