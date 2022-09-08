2584c0385e6221dbedecf7733d3c07914f0d8575
Lewis Sparlin <hello@lewismsparlin.com>
2022-09-08 14:11:36 -0500

    Add sanity config options as ENV vars

--- config/initializers/sanity.rb
+++ config/initializers/sanity.rb
@@ -1,7 +1,7 @@
 Sanity.configure do |config|
-  config.token = 'sk9xVPLpl3Cc2u5Qe58Qioa1dQOoMnLc4z0pe7PWOFd19EixynqZyetW95cnTUODQLuVxbDFdgNbwmvHNDNPzJJ6fAQXhDj1YLmPhBQfdWX36qBDMLu4cisytC1wlyr9G5kGUoaJIpMFp1T6m60y8rWZ3xVr9515YKj37SRMU1FokRbJXkwn'
-  config.project_id = 'qk39exus'
+  config.token = ENV['SANITY_TOKEN']
+  config.project_id = ENV['SANITY_PROJECT_ID']
   config.api_version = 'v2021-03-25'
-  config.dataset = 'production'
+  config.dataset = ENV['SANITY_DATASET'] || 'production'
   config.use_cdn = true
 end
