ea81cb1d1dec872b2b765ecbaac3822032f27c2d
Lewis Sparlin <hello@lewismsparlin.com>
2023-08-26 22:02:56 -0500

    Remove fragility causing crash when new sanity fields to settings

--- app/lib/sanity_content/site_settings_query.rb
+++ app/lib/sanity_content/site_settings_query.rb
@@ -1,21 +1,37 @@
 class SanityContent::SiteSettingsQuery < Sanity::Resource
   queryable
 
-  def self.find_only
-    where(groq: query).dig("result")
-  end
+  class << self
+    def find_only
+      where(groq: query).dig("result")
+    end
 
-  def self.query
-    <<-GROQ
-  *[_type == 'siteSettings'][0] {
-    ...,
-    "og_image_url": og_image.asset->url,
-    socialLinks[] {
+    def query
+      <<-GROQ
+      *[_type == 'siteSettings'][0] {
+        #{fields}
+      }
+      GROQ
+    end
+
+    def fields
+      <<-GROQ
+      _id,
+      _type,
+      _createdAt,
       title,
-      url,
-      "icon_url": icon.asset->url,
-    },
-  }
-    GROQ
+      subtitle,
+      bio,
+      description,
+      keywords,
+      twitter_account,
+      "og_image_url": og_image.asset->url,
+      socialLinks[] {
+        title,
+        url,
+        "icon_url": icon.asset->url,
+      },
+      GROQ
+    end
   end
 end
